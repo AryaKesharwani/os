57e181342c2555fbfce72ec35ec8c6ce30494d5c
Arya kesarwani <aryakesharwani1234@gmail.com>
2023-04-24 00:45:01 +0530

    process_creation

---
+++ process_creation/w4q7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(int argc, char const *argv[])
+{
+    int n;
+    int i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        fork();
+    return 0;
+}
